402f7ee649523512ace99d63721cc463207bf449
Ernesto Canquiz <caribestic123@gmail.com>
2024-10-25 16:24:39 -0400

    feat: Scaffolding Home, Login, Register and Dashboard components

--- src/layouts/Sidebar.vue
+++ src/layouts/Sidebar.vue
@@ -25,70 +25,104 @@ const { isOpen, isClose } = useSidebar()
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
       class="mt-10 bg-base-200 fixed z-50 inset-y-0 left-0 transition duration-300 transform overflow-y-auto lg:static lg:inset-0 block lg:hidden"
     >
-    <nav class="py-6 px-6">
-      <div 
-      class="mb-2 py-1 px-2"
-      :class="route.path.startsWith(`/`) ? 'activeClass' : 'inactiveClass'">      
-    <AppLink :to="'/'">
-        <span class="flex items-center group py-0">
-          <!--Component v-if="menu && menu.icon!==''"
-            :is="iconName"
-            class="w-5 h-5 mr-2 fill-current"
-          /-->
-          Todos los movimientos
-        </span>
-      </AppLink>
-      </div>
-      <div 
-      class="mb-2 py-1 px-2"
-      :class="route.path.startsWith(`/edit/`) ? 'activeClass' : 'inactiveClass'">
+      <nav class="py-6 px-6">
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/dashboard`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/dashboard'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Dashboard
+            </span>
+          </AppLink>
+        </div>
 
-      <AppLink :to="'/edit/1'">
-        <span class="flex items-center group py-0">
-          <!--Component v-if="menu && menu.icon!==''"
-            :is="iconName"
-            class="w-5 h-5 mr-2 fill-current"
-          /-->
-          Editar el primero
-        </span>
-      </AppLink>
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/movements`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/movements'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Todos los movimientos
+            </span>
+          </AppLink>
+        </div>
+
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/movements/edit/`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/movements/edit/1'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Editar el primero
+            </span>
+          </AppLink>
+        </div>      
+      </nav> 
     </div>
-      
-    </nav> 
-    </div>
+
     <div
       :class="!isClose ? 'translate-x-0 ease-out w-full' : '-translate-x-full ease-in w-0'"
       class="bg-base-200 fixed z-30 inset-y-0 left-0 transition duration-300 transform overflow-y-auto lg:static lg:inset-x-0 hidden lg:block"
     >
-    <nav class="py-6 px-6">
-      <div 
-      class="mb-2 py-1 px-2"
-      :class="route.path.startsWith(`/`) ? 'activeClass' : 'inactiveClass'">      
-      <AppLink :to="'/'">
-        <span class="flex items-center group py-0">
-          <!--Component v-if="menu && menu.icon!==''"
-            :is="iconName"
-            class="w-5 h-5 mr-2 fill-current"
-          /-->
-          Todos los movimientos
-        </span>
-      </AppLink>
-      </div>
-      <div 
-      class="mb-2 py-1 px-2"
-      :class="route.path.startsWith(`/edit/`) ? 'activeClass' : 'inactiveClass'">
-      <AppLink :to="'/edit/1'">
-        <span class="flex items-center group py-0">
-          <!--Component v-if="menu && menu.icon!==''"
-            :is="iconName"
-            class="w-5 h-5 mr-2 fill-current"
-          /-->
-          Editar el primero
-        </span>
-      </AppLink>
-    </div>
-      
-    </nav> 
+      <nav class="py-6 px-6">
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/dashboard`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/dashboard'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Dashboard
+            </span>
+          </AppLink>
+        </div>
+
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/movements`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/movements'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Todos los movimientos
+            </span>
+          </AppLink>
+        </div>
+
+        <div 
+          class="mb-2 py-1 px-2"
+          :class="route.path.startsWith(`/movements/edit/`) ? 'activeClass' : 'inactiveClass'"
+        >
+          <AppLink :to="'/movements/edit/1'">
+            <span class="flex items-center group py-0">
+              <!--Component v-if="menu && menu.icon!==''"
+                :is="iconName"
+                class="w-5 h-5 mr-2 fill-current"
+              /-->
+              Editar el primero
+            </span>
+          </AppLink>
+        </div>      
+      </nav> 
     </div>
   </div>
 </template>
